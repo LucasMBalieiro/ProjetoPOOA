1ad85e20f4498e55c1509cf930e934110892d005
vini <viniromualdo082@gmail.com>
2023-07-03 19:46:19 -0300

    poucas alteracoes

--- src/main/java/br/ufscar/pooa/model/deposito/estoque/itens/Material.java
+++ src/main/java/br/ufscar/pooa/model/deposito/estoque/itens/Material.java
@@ -1,6 +1,8 @@
 package br.ufscar.pooa.model.deposito.estoque.itens;
 
-public class Material extends ItemDeEstoque {
+import br.ufscar.pooa.model.deposito.estoque.Utilizavel;
+
+public class Material extends ItemDeEstoque implements Utilizavel {
   private String quantidade;
 
   public Material(String marca, String modelo, int quantidadeDeEstoque, double valor) {
--- src/main/java/br/ufscar/pooa/model/deposito/estoque/itens/Peca.java
+++ src/main/java/br/ufscar/pooa/model/deposito/estoque/itens/Peca.java
@@ -1,8 +1,10 @@
 package br.ufscar.pooa.model.deposito.estoque.itens;
 
+import br.ufscar.pooa.model.deposito.estoque.Utilizavel;
+
 import java.util.List;
 
-public class Peca extends ItemDeEstoque {
+public class Peca extends ItemDeEstoque implements Utilizavel {
   private List<Equipamento> equipamentos;
 
   public Peca(String marca, String modelo, int quantidadeDeEstoque, double valor) {
