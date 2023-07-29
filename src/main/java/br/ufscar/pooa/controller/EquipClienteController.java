2b4b853bde155377c7bbbbb80a2af6a9e8795a50
vini <viniromualdo082@gmail.com>
2023-07-29 15:20:20 -0300

    codigo-fonte fiel ao diagrama 1

--- src/main/java/br/ufscar/pooa/controller/EquipClienteController.java
+++ src/main/java/br/ufscar/pooa/controller/EquipClienteController.java
@@ -1,62 +1,62 @@
-package br.ufscar.pooa.controller;
-
-import br.ufscar.pooa.model.servicos.EquipamentoAceitoParaServico;
-import br.ufscar.pooa.model.servicos.equipamentocliente.EquipamentoCliente;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class EquipClienteController {
-  private List<EquipamentoCliente> equipamentosClientes;
-
-  public EquipClienteController() {
-    equipamentosClientes = new ArrayList<>();
-  }
-
-  public void adicionar(EquipamentoCliente equipamentoCliente) {
-    equipamentosClientes.add(equipamentoCliente);
-  }
-
-  public void remover(EquipamentoCliente equipamentoCliente) {
-    equipamentosClientes.remove(equipamentoCliente);
-  }
-
-  public void atualizar(EquipamentoCliente equipamentoCliente, EquipamentoAceitoParaServico novoEquipamento) {
-
-    for (EquipamentoCliente equipCliente : equipamentosClientes) {
-      if (equipCliente.equals(equipamentoCliente)) {
-        equipCliente.setTipo(novoEquipamento);
-        return;
-      }
-    }
-  }
-
-  public List<EquipamentoCliente> obterPorModelo(String modelo) {
-    List<EquipamentoCliente> equipamentosClientesPorModelo = new ArrayList<>();
-
-    for (EquipamentoCliente equipamentoCliente : equipamentosClientes) {
-      if (equipamentoCliente.getTipo().getModelo().equals(modelo)) {
-        equipamentosClientesPorModelo.add(equipamentoCliente);
-      }
-    }
-
-    return equipamentosClientesPorModelo;
-  }
-
-  public List<EquipamentoCliente> obterPorMarca(String marca) {
-    List<EquipamentoCliente> equipamentosClientesPorMarca = new ArrayList<>();
-
-    for (EquipamentoCliente equipamentoCliente : equipamentosClientes) {
-      if (equipamentoCliente.getTipo().getMarca().equals(marca)) {
-        equipamentosClientesPorMarca.add(equipamentoCliente);
-      }
-    }
-
-    return equipamentosClientesPorMarca;
-  }
-
-
-  public List<EquipamentoCliente> obterTodosEquip() {
-    return equipamentosClientes;
-  }
-}
+//package br.ufscar.pooa.controller;
+//
+//import br.ufscar.pooa.model.servicos.EquipamentoAceitoParaServico;
+//import br.ufscar.pooa.model.servicos.equipamentocliente.EquipamentoCliente;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class EquipClienteController {
+//  private List<EquipamentoCliente> equipamentosClientes;
+//
+//  public EquipClienteController() {
+//    equipamentosClientes = new ArrayList<>();
+//  }
+//
+//  public void adicionar(EquipamentoCliente equipamentoCliente) {
+//    equipamentosClientes.add(equipamentoCliente);
+//  }
+//
+//  public void remover(EquipamentoCliente equipamentoCliente) {
+//    equipamentosClientes.remove(equipamentoCliente);
+//  }
+//
+//  public void atualizar(EquipamentoCliente equipamentoCliente, EquipamentoAceitoParaServico novoEquipamento) {
+//
+//    for (EquipamentoCliente equipCliente : equipamentosClientes) {
+//      if (equipCliente.equals(equipamentoCliente)) {
+//        equipCliente.setTipo(novoEquipamento);
+//        return;
+//      }
+//    }
+//  }
+//
+//  public List<EquipamentoCliente> obterPorModelo(String modelo) {
+//    List<EquipamentoCliente> equipamentosClientesPorModelo = new ArrayList<>();
+//
+//    for (EquipamentoCliente equipamentoCliente : equipamentosClientes) {
+//      if (equipamentoCliente.getTipo().getModelo().equals(modelo)) {
+//        equipamentosClientesPorModelo.add(equipamentoCliente);
+//      }
+//    }
+//
+//    return equipamentosClientesPorModelo;
+//  }
+//
+//  public List<EquipamentoCliente> obterPorMarca(String marca) {
+//    List<EquipamentoCliente> equipamentosClientesPorMarca = new ArrayList<>();
+//
+//    for (EquipamentoCliente equipamentoCliente : equipamentosClientes) {
+//      if (equipamentoCliente.getTipo().getMarca().equals(marca)) {
+//        equipamentosClientesPorMarca.add(equipamentoCliente);
+//      }
+//    }
+//
+//    return equipamentosClientesPorMarca;
+//  }
+//
+//
+//  public List<EquipamentoCliente> obterTodosEquip() {
+//    return equipamentosClientes;
+//  }
+//}
--- src/main/java/br/ufscar/pooa/controller/PecaController.java
+++ src/main/java/br/ufscar/pooa/controller/PecaController.java
@@ -1,64 +1,64 @@
-package br.ufscar.pooa.controller;
-
-import br.ufscar.pooa.model.armazenamento.estoque.itens.Peca;
-import br.ufscar.pooa.model.armazenamento.estoque.itens.Equipamento;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class PecaController {
-  private List<Peca> pecas;
-
-  public PecaController() {
-    this.pecas = new ArrayList<>();
-  }
-
-  public void adicionar(Peca peca) {
-    pecas.add(peca);
-  }
-
-  public void remover(Peca peca) {
-    pecas.remove(peca);
-  }
-
-  public void atualizar(Peca peca) {
-    for (int i = 0; i < pecas.size(); i++) {
-      if (pecas.get(i).equals(peca)) {
-        pecas.set(i, peca);
-        return;
-      }
-    }
-  }
-
-  public List<Peca> obterPorModelo(String modelo) {
-    List<Peca> pecasPorModelo = new ArrayList<>();
-    // Filtra as peças pelo modelo
-    for (Peca peca : pecas) {
-      if (peca.getModelo().equals(modelo)) {
-        pecasPorModelo.add(peca);
-      }
-    }
-    return pecasPorModelo;
-  }
-
-  public List<Peca> obterPorMarca(String marca) {
-    List<Peca> pecasPorMarca = new ArrayList<>();
-    // Filtra as peças pela marca
-    for (Peca peca : pecas) {
-      if (peca.getMarca().equals(marca)) {
-        pecasPorMarca.add(peca);
-      }
-    }
-    return pecasPorMarca;
-  }
-
-  public List<Equipamento> obterEquipamentos() {
-    List<Equipamento> equipamentos = new ArrayList<>();
-    // Obtém todos os equipamentos das peças
-    for (Peca peca : pecas) {
-      equipamentos.addAll(peca.getEquipamentos());
-    }
-    return equipamentos;
-  }
-}
-
+//package br.ufscar.pooa.controller;
+//
+//import br.ufscar.pooa.model.armazenamento.estoque.itens.Peca;
+//import br.ufscar.pooa.model.armazenamento.estoque.itens.Equipamento;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class PecaController {
+//  private List<Peca> pecas;
+//
+//  public PecaController() {
+//    this.pecas = new ArrayList<>();
+//  }
+//
+//  public void adicionar(Peca peca) {
+//    pecas.add(peca);
+//  }
+//
+//  public void remover(Peca peca) {
+//    pecas.remove(peca);
+//  }
+//
+//  public void atualizar(Peca peca) {
+//    for (int i = 0; i < pecas.size(); i++) {
+//      if (pecas.get(i).equals(peca)) {
+//        pecas.set(i, peca);
+//        return;
+//      }
+//    }
+//  }
+//
+//  public List<Peca> obterPorModelo(String modelo) {
+//    List<Peca> pecasPorModelo = new ArrayList<>();
+//    // Filtra as peças pelo modelo
+//    for (Peca peca : pecas) {
+//      if (peca.getModelo().equals(modelo)) {
+//        pecasPorModelo.add(peca);
+//      }
+//    }
+//    return pecasPorModelo;
+//  }
+//
+//  public List<Peca> obterPorMarca(String marca) {
+//    List<Peca> pecasPorMarca = new ArrayList<>();
+//    // Filtra as peças pela marca
+//    for (Peca peca : pecas) {
+//      if (peca.getMarca().equals(marca)) {
+//        pecasPorMarca.add(peca);
+//      }
+//    }
+//    return pecasPorMarca;
+//  }
+//
+//  public List<Equipamento> obterEquipamentos() {
+//    List<Equipamento> equipamentos = new ArrayList<>();
+//    // Obtém todos os equipamentos das peças
+//    for (Peca peca : pecas) {
+//      equipamentos.addAll(peca.getEquipamentos());
+//    }
+//    return equipamentos;
+//  }
+//}
+//
