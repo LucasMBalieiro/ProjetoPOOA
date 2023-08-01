0dc9bbb2a7f1104f44bfe9c5c46cb71b0361ead7
vini <viniromualdo082@gmail.com>
2023-08-01 14:26:52 -0300

    pequena alteracao em Cliente.java

--- src/main/java/br/ufscar/pooa/model/pessoas/Cliente.java
+++ src/main/java/br/ufscar/pooa/model/pessoas/Cliente.java
@@ -1,10 +1,5 @@
 package br.ufscar.pooa.model.pessoas;
 
-import br.ufscar.pooa.model.servicos.equipamentocliente.EquipamentoCliente;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Cliente extends Pessoa {
 
   public Cliente(String nome, String cpf, String telefone, String email, String endereco) {
